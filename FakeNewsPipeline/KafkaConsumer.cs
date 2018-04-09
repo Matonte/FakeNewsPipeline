caa1fe440a90bbfdd90db067e67904dbedc6289d
Matonte <matonte3@gmail.com>
2018-04-09 14:00:07 -0400

    my pipeline

---
+++ FakeNewsPipeline/KafkaConsumer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KafkaNet;
+using KafkaNet.Model;
+using KafkaNet.Protocol;
+
+namespace KafkaConsumer
+{
+    class Program
+    {
+        static void kafkaConsumer()
+        {
+            OffsetPosition[] offsetPositions = new OffsetPosition[]
+       {
+            new OffsetPosition()
+            {
+               Offset = 1,
+               PartitionId = 0
+            }
+       };
+            var options = new KafkaOptions(new Uri("http://localhost:9092"),
+            new Uri("http://localhost:9092"));
+            var consumer = new KafkaNet.Consumer(new ConsumerOptions("test",
+               new BrokerRouter(options)), offsetPositions);
+
+
+            using (FileStream fileStream = new FileStream("tweets.txt", FileMode.OpenOrCreate))
+            {
+                using (StreamWriter writer = new StreamWriter(fileStream))
+                {
+                    using (TextWriter originalConsoleOut = Console.Out)
+                    {
+                        foreach (var message in consumer.Consume())
+                        {
+                            Console.WriteLine("Response: P{0},O{1} : {2}",
+                               message.Meta.PartitionId, message.Meta.Offset,
+                            Encoding.UTF8.GetString(message.Value));
+                            Console.SetOut(writer);
+                            Console.WriteLine(originalConsoleOut);
+                        }
+                    }
+                }
+                Console.WriteLine("Hello to console only");
+
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            kafkaConsumer();
+        }
+    }
+}
---
+++ FakeNewsPipeline/TwitterProducer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tweetinvi;
+using KafkaNet;
+using KafkaNet.Model;
+using KafkaNet.Protocol;
+using System.IO;
+
+namespace TwitterApp
+{
+    class Program
+    {
+
+       public static void twitterInfo()
+        {
+            var options = new KafkaOptions
+           (new Uri("http://localhost:9092"));
+            var router = new BrokerRouter(options);
+            var client = new KafkaNet.Producer(router);
+
+          const String topic = "test";
+	 	  const   String tokenSecret = "FbcP68UtZR8U6n0AIMkBZHxgx4hzv3ibLQrU35qQipQ7Y";
+		  const   String consumerSecret = "CqsVHqSKW4S6hH6eGrq9B4PvKCZZElJ8XLFMeJihEouyJHl9xH";
+	      const   String token = "2710082868-swCbtRmmODBOB6TMbsDGIQNMCUKoATTVAwbCQwi";
+	      const   String consumerKey = "XC6GdAJVYF9jMQqS68bQOu6kG";
+
+        Auth.SetUserCredentials(consumerKey, consumerSecret, token, tokenSecret);
+             var stream = Tweetinvi.Stream.CreateFilteredStream();
+             stream.AddTrack("Politics");
+
+            stream.MatchingTweetReceived += (sender, arguments) =>
+          {
+       
+           
+
+              client.SendMessageAsync("test", new[]
+                   { new  KafkaNet.Protocol.Message("tweet:  " + arguments.Tweet.Text) }).Wait();
+
+          };
+            stream.StartStreamMatchingAllConditions();
+        }
+
+    static void Main(string[] args)
+        {
+            twitterInfo();
+        }
+    }
+}
